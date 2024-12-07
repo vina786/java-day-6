eb802dd8a5d175e0cb6fc91233219e2eca6ae888
vina786 <vinay76b@gmail.com>
2024-12-07 16:11:08 +0530

    Add files via upload

---
+++ hallowright.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class hallowright{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(j==0 || i==n-1 || i==j)
+                System.out.print("*"+" ");
+                else{
+            
+            System.out.print(" "+" ");
+                }
+        }System.out.println();
+    }}
+}
