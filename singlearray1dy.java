679f7478c541dc0e38d751ad976d7ae90f18b8b0
vina786 <vinay76b@gmail.com>
2024-12-07 16:15:01 +0530

    Add files via upload

---
+++ singlearray1dy.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class singlearray1dy {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+            System.out.println(Arrays.toString(arr));
+        }
+    }
+}
