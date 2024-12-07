28c04223daea046365073d198917249c41695126
vina786 <vinay76b@gmail.com>
2024-12-07 16:10:51 +0530

    Add files via upload

---
+++ hallowleft.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+public class hallowleft{
+      public static void main(String args[])
+{
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(j==n-1 || i+j==n-1 || i==n-1)
+            System.out.print("*"+" ");
+            else{
+        
+        System.out.print(" "+" ");
+            }
+    }System.out.println();
+}}
+}
