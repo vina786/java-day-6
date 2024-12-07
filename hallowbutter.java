b852d8bbdabba54d923cd804d0df5b41dff96a99
vina786 <vinay76b@gmail.com>
2024-12-07 16:10:10 +0530

    Add files via upload

---
+++ hallowbutter.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class hallowbutter
+{
+  public static void main(String args[])
+  {
+    int n=5;
+    for(int i=0;i<n-1;i++)
+    {
+      for(int j=0;j<n-1;j++)
+      {
+        if(j==0 || i==j)
+        System.out.print("* ");
+        else
+        System.out.print("  ");
+      }
+      System.out.println();
+    }
+    for(int i=0;i<n;i++)
+    {
+      for(int j=0;j<n;j++)
+      {
+        if(i==0 || j==0 || i+j==n-1)
+        System.out.print("* ");
+        else
+        System.out.print("  ");
+      }
+      System.out.println();
+    }
+  }
+}
