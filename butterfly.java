630b3d243ba3351d53935a3bf8ea4eff9d889ccd
vina786 <vinay76b@gmail.com>
2024-12-07 16:09:44 +0530

    Add files via upload

---
+++ butterfly.java
@@ -0,0 +1,18 @@
+public class butterfly
+{
+  public static void main(String args[])
+  {
+    int n=9;
+    for(int i=0;i<n;i++)
+    {
+      for(int j=0;j<n;j++)
+      {
+        if(j==0 || i==j || j==n-1 || i+j==n-1) 
+        System.out.print("* ");
+        else
+        System.out.print("  ");
+      }
+      System.out.println();
+    }
+  }
+}
