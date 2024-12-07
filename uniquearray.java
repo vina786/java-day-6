0e6d9a16502c9777ab9086743766be5f133da0cc
vina786 <vinay76b@gmail.com>
2024-12-07 16:15:27 +0530

    Add files via upload

---
+++ uniquearray.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class uniquearray
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+            ans^=arr[i];
+        }
+        System.out.println(ans);
+        }
+
+    }
