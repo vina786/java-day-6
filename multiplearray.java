0eb5403b2d3a9e02831c22d9cfed27aebd8589ae
vina786 <vinay76b@gmail.com>
2024-12-07 16:12:19 +0530

    Add files via upload

---
+++ multiplearray.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class multiplearray {
+     public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        int s=1;
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+            s*=arr[i];
+        }
+        System.out.println(s);
+        }
+    }
+
